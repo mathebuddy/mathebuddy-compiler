ad8213b540fb58474ca7fbefa8a63ddc29bd60ed
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-09-02 14:49:44 +0200

    parsing and code generation updates

--- src/parse.ts
+++ src/parse.ts
@@ -6,13 +6,76 @@
  * License: GPL-3.0-or-later
  */
 
-import { Course } from './data';
+import { Course, DocumentItem, Exercise } from './data';
 import { CourseDocument } from './data';
+import { Lexer } from 'multila-lexer/src/lex';
+import { SMPL } from 'mathebuddy-smpl/src/index';
 
 class Block {
   type = '';
+  title = '';
   label = '';
   parts: BlockPart[] = [];
+  srcLine = 0;
+
+  private parser: Parser = null;
+
+  constructor(parser: Parser) {
+    this.parser = parser;
+  }
+
+  process(): DocumentItem {
+    //console.log('BLOCK TYPE: ' + this.type);
+    //console.log('BLOCK TITLE: ' + this.title);
+    //console.log('BLOCK LABEL: ' + this.label);
+    //console.log('');
+    if (this.type === 'EXERCISE') {
+      const exercise = new Exercise();
+      exercise.title = this.title;
+      for (const part of this.parts) {
+        if (part.name === 'code') {
+          exercise.code_raw = part.lines.join('\n');
+        } else if (part.name === 'text') {
+          exercise.text_raw = part.lines.join('\n');
+        }
+        // TODO: else: warning/error
+      }
+
+      // TODO: catch errors
+      try {
+        const variables = SMPL.interpret(exercise.code_raw);
+        for (const local of variables) {
+          console.log(local.id + ' = ' + local.value.toString());
+        }
+      } catch (e) {
+        this.error(e.toString());
+      }
+
+      /*const sc = new SellCode();
+      try {
+        const generatedCode = sc.parse(exercise.code_raw);
+        const int = new SellInterpreter();
+
+        // interpret multiple times to get various exercises
+        int.interpret(generatedCode, sc.getLocalSymbols());
+        for (const local of sc.getLocalSymbols()) {
+          console.log(local.id + ' = ' + local.value.toString());
+        }
+      } catch (e) {
+        this.error(e.toString());
+      }*/
+      return exercise;
+    } /*TODO: else {
+      this.error('unknown block type "' + this.type + '"');
+      // -> TODO: just a warning and do not process just this block??
+    }*/
+    return null;
+  }
+
+  private error(message: string): void {
+    console.error('ERROR:' + (this.srcLine + 1) + ': ' + message);
+    process.exit(-1);
+  }
 }
 
 class BlockPart {
@@ -28,16 +91,6 @@ export class Parser {
   private i = -1; // current line index (starting from 0)
   private line = ''; // current line
   private line2 = ''; // next line
-
-  private next(): void {
-    this.i++;
-    if (this.i < this.srcLines.length) {
-      this.line = this.srcLines[this.i];
-    } else this.line = '§END';
-    if (this.i + 1 < this.srcLines.length) {
-      this.line2 = this.srcLines[this.i + 1];
-    } else this.line2 = '§END';
-  }
 
   public getCourse(): Course {
     return this.course;
@@ -63,13 +116,23 @@ export class Parser {
     while (this.line !== '§END') {
       if (this.line.length == 0) this.next();
       else if (this.line2.startsWith('###')) this.parseDocumentTitle();
-      else if (this.line.startsWith('---')) this.parseBlock();
-      //else this.error('unexpected tokens');
+      else if (this.line === '---') this.parseBlock();
       else this.parseParagraph();
     }
+    const bp = 1337;
   }
 
-  // documentTitle = { CHAR } "@" { ID } NEWLINE "###" { "#" } NEWLINE;
+  private next(): void {
+    this.i++;
+    if (this.i < this.srcLines.length) {
+      this.line = this.srcLines[this.i];
+    } else this.line = '§END';
+    if (this.i + 1 < this.srcLines.length) {
+      this.line2 = this.srcLines[this.i + 1];
+    } else this.line2 = '§END';
+  }
+
+  //G documentTitle = { CHAR } "@" { ID } NEWLINE "###" { "#" } NEWLINE;
   private parseDocumentTitle(): void {
     const tokens = this.line.split('@');
     this.document.title = tokens[0].trim();
@@ -78,42 +141,60 @@ export class Parser {
     this.next(); // skip '############'
   }
 
-  // block = "---" NEWLINE { "@" ID NEWLINE | LINE } "---" NEWLINE;
+  //G block = "---" NEWLINE { "@" ID NEWLINE | LINE } "---" NEWLINE;
   private parseBlock(): void {
-    const block = new Block();
+    const block = new Block(this);
+    block.srcLine = this.i;
     this.next(); // skip "---"
-    const tokens = this.line.split('@');
-    block.type = tokens[0];
-    if (tokens.length > 1) block.label = tokens[1];
+    const tokens = this.line.split(' ');
+    for (let k = 0; k < tokens.length; k++) {
+      if (k == 0) block.type = tokens[k];
+      else if (tokens[k].startsWith('@')) block.label = tokens[k];
+      else block.title += tokens[k] + ' ';
+    }
+    block.title = block.title.trim();
+    //block.type = tokens[0];
+    //if (tokens.length > 1) block.label = tokens[1];
     this.next();
     let part: BlockPart = new BlockPart();
     part.name = 'global';
     block.parts.push(part);
-    while (this.line.startsWith('---') == false && this.line !== '§END') {
+    while (this.line !== '---' && this.line !== '§END') {
       if (this.line.startsWith('@')) {
         part = new BlockPart();
         block.parts.push(part);
-        part.name = this.line.substring(1);
+        part.name = this.line.substring(1).trim();
         this.next();
       } else {
         part.lines.push(this.line);
         this.next();
       }
     }
-    if (this.line.startsWith('---')) this.next();
-    else this.error('block must end with ---');
+    if (this.line === '---') this.next();
+    else
+      this.error(
+        'block started in line ' + block.srcLine + ' must end with ---',
+      );
+    const docItem = block.process();
+    if (docItem != null) this.document.items.push(docItem);
   }
 
-  // paragraph =
-  //     { paragraphPart };
-  // paragraphPart =
-  //     ID
-  //   | "**" paragraph('bold') "**"
-  //   | "*" paragraph('italic') "*"
-  //   | "[" paragraph('format') "]" "@" ID
-  //   | "$" inlineMath('math') "$";
+  /*G paragraph =
+       { paragraphPart };
+   paragraphPart =
+       ID
+     | "**" paragraph('bold') "**"
+     | "*" paragraph('italic') "*"
+     | "[" paragraph('format') "]" "@" ID
+     | "$" inlineMath('math') "$";
+  */
   private parseParagraph(mode = ''): void {
-    //const lexer = new Lexer(this.line);
+    const lexer = new Lexer();
+
+    lexer.pushSource('', this.line);
+
+    this.next();
+
     const bp = 1337;
   }
 
