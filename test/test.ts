7d6e317a8331f8d73d2bcbe9b2c99ddf4798f9f3
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-10-04 14:44:54 +0200

    Update test.ts

--- test/test.ts
+++ test/test.ts
@@ -8,6 +8,7 @@
 
 import * as fs from 'fs';
 import { Parser } from '../src';
+import * as lz_string from 'lz-string';
 
 console.log('mathe:buddy Compiler (c) 2022 by TH Koeln');
 
@@ -19,3 +20,12 @@ parser.run(src);
 
 const output = JSON.stringify(parser.getCourse().toJSON(), null, 2);
 console.log(output);
+
+const outputPath = inputPath + '_COMPILED.json';
+fs.writeFileSync(outputPath, output);
+
+const output_compressed = JSON.stringify(parser.getCourse().toJSON());
+const output_compressed_lz = lz_string.compress(output_compressed);
+
+const outputPath_COMPRESSED = inputPath + '_COMPILED_COMPRESSED.hex';
+fs.writeFileSync(outputPath_COMPRESSED, output_compressed_lz);
