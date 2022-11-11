db31590993b72be5d5373fc76e31157e5c425543
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-11-11 09:21:54 +0100

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -8,6 +8,7 @@
 
 import * as esbuild from 'esbuild';
 import { execSync } from 'child_process';
+import * as fs from 'fs';
 
 // ---- build ----
 esbuild.buildSync({
@@ -21,12 +22,24 @@ esbuild.buildSync({
 });
 
 // ---- convert README.md to README.html ----
+
 const date = new Date().toISOString().slice(0, 10);
 execSync("sed -e '1,/<!-- start-for-website -->/d' mbl.md > __tmp.md");
 execSync(
-  'pandoc -s __tmp.md --metadata title="MATHE:BUDDY LANGUAGE (MBL)" --metadata author="" --metadata date="' +
+  'pandoc -s __tmp.md --metadata title="Mathe:Buddy Language (MBL)" --metadata author="" --metadata date="' +
     date +
     '"  --css README.css --embed-resources --standalone -o mbl.html',
 );
 execSync('rm __tmp.md');
+
 // TODO: --mathjax may be needed, but results in large file, if --self-contained option is provided...
+
+const title = `<h1 class="title">`;
+const links =
+  `[<a href="https://app.f07-its.fh-koeln.de">Home</a>] ` +
+  `[<a href="https://app.f07-its.fh-koeln.de/docs-mbl.html">MBL Reference</a>] ` +
+  `[<a href="https://app.f07-its.fh-koeln.de/docs-smpl.html">SMPL Reference</a>]`;
+fs.writeFileSync(
+  'mbl.html',
+  fs.readFileSync('mbl.html', 'utf-8').replace(title, links + title),
+);
