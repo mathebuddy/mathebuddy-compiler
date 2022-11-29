38a95b4a56cda18ba6cf9c5f2c37cf94e92f7827
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-11-29 11:17:43 +0100

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -25,26 +25,44 @@ esbuild.buildSync({
 
 execSync('python3 examples/_get.py');
 
-// ---- convert README.md to README.html ----
+// ---- convert Markdown files to HTML files ----
 
 const date = new Date().toISOString().slice(0, 10);
-execSync("sed -e '1,/<!-- start-for-website -->/d' mbl.md > __tmp.md");
-execSync(
-  'pandoc -s __tmp.md --metadata title="Mathe:Buddy Language (MBL)" --metadata author="" --metadata date="' +
-    date +
-    '"  --css README.css --embed-resources --standalone -o mbl.html',
-);
-execSync('rm __tmp.md');
 
-// TODO: --mathjax may be needed, but results in large file, if --self-contained option is provided...
+const files = ['mbl', 'mbcl'];
+const titles = [
+  'Mathe:Buddy Language (MBL)',
+  'Mathe:Buddy Compiled Language (MBCL)',
+];
 
-const title = `<h1 class="title">`;
-const links =
-  `[<a href="https://app.f07-its.fh-koeln.de">Home</a>] ` +
-  `[<a href="https://app.f07-its.fh-koeln.de/docs-mbl.html">MBL Reference</a>] ` +
-  `[<a href="https://app.f07-its.fh-koeln.de/docs-smpl.html">SMPL Reference</a>] ` +
-  `[<a href="https://app.f07-its.fh-koeln.de/docs-sim.html">Installation</a>] `;
-fs.writeFileSync(
-  'mbl.html',
-  fs.readFileSync('mbl.html', 'utf-8').replace(title, links + title),
-);
+for (let i = 0; i < files.length; i++) {
+  const file = files[i];
+  const title = titles[i];
+  execSync(
+    "sed -e '1,/<!-- start-for-website -->/d' " + file + '.md > __tmp.md',
+  );
+  execSync(
+    'pandoc -s __tmp.md --metadata title="' +
+      title +
+      '" --metadata author="" --metadata date="' +
+      date +
+      '"  --css README.css --embed-resources --standalone -o ' +
+      file +
+      '.html',
+  );
+  execSync('rm __tmp.md');
+
+  // TODO: --mathjax may be needed, but results in large file, if --self-contained option is provided...
+
+  const _title = `<h1 class="title">`;
+  const links =
+    `[<a href="https://app.f07-its.fh-koeln.de">Home</a>] ` +
+    `[<a href="https://app.f07-its.fh-koeln.de/docs-mbl.html">MBL Reference</a>] ` +
+    `[<a href="https://app.f07-its.fh-koeln.de/docs-smpl.html">SMPL Reference</a>] ` +
+    `[<a href="https://app.f07-its.fh-koeln.de/docs-mbcl.html">MBCL Reference</a>] ` +
+    `[<a href="https://app.f07-its.fh-koeln.de/docs-sim.html">Installation</a>] `;
+  fs.writeFileSync(
+    file + '.html',
+    fs.readFileSync(file + '.html', 'utf-8').replace(_title, links + _title),
+  );
+}
