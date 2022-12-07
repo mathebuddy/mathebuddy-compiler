d57eabebfb344eba181f4a8807dd04c2aa13b76e
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-07 18:01:17 +0100

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -6,10 +6,7 @@
  * License: GPL-3.0-or-later
  */
 
-// TODO: use npm-package in future!
-import { Matrix } from '@mathebuddy/mathebuddy-smpl/src/matrix';
-import { Set_INT } from '@mathebuddy/mathebuddy-smpl/src/set';
-import { BaseType, SymTabEntry } from '@mathebuddy/mathebuddy-smpl/src/symbol';
+// refer to the specification at https://app.f07-its.fh-koeln.de/docs-mbcl.html
 
 export type JSONValue =
   | string
@@ -18,356 +15,507 @@ export type JSONValue =
   | { [x: string]: JSONValue }
   | Array<JSONValue>;
 
-export class Course {
-  id = '';
-  author = '';
-  modifiedDate = '';
-  documents: CourseDocument[] = [];
+// -------- COURSE --------
 
+export class MBL_Course {
+  title = '';
+  author = '';
+  mbcl_version = 1;
+  date_modified = Date.now();
+  chapters: MBL_Chapter[] = [];
   toJSON(): JSONValue {
-    const docs: JSONValue = [];
-    for (const d of this.documents) {
-      docs.push(d.toJSON());
-    }
     return {
-      id: this.id,
+      title: this.title,
       author: this.author,
-      modifiedDate: this.modifiedDate,
-      documents: docs,
+      mbcl_version: this.mbcl_version,
+      date_modified: this.date_modified,
+      chapters: this.chapters.map((chapter) => chapter.toJSON()),
     };
   }
 }
 
-export class CourseDocument {
+// -------- CHAPTER --------
+
+export class MBL_Chapter {
   title = '';
   alias = '';
-  items: DocumentItem[] = [];
-
+  levels: MBL_Level[] = [];
   toJSON(): JSONValue {
-    const items: JSONValue = [];
-    for (const item of this.items) {
-      items.push(item.toJSON());
-    }
     return {
       title: this.title,
       alias: this.alias,
-      items: items,
+      levels: this.levels.map((level) => level.toJSON()),
     };
   }
 }
 
-export abstract class DocumentItem {
+// -------- LEVEL --------
+
+export class MBL_Level {
+  title = '';
+  alias = '';
+  pos_x = -1;
+  pos_y = -1;
+  requires: MBL_Level[] = [];
+  items: MBL_LevelItem[] = [];
   toJSON(): JSONValue {
-    return {}; // overwritten by derived classes
+    return {
+      title: this.title,
+      alias: this.alias,
+      pos_x: this.pos_x,
+      pos_y: this.pos_y,
+      requires: this.requires.map((req) => req.title),
+      items: this.items.map((item) => item.toJSON()),
+    };
   }
 }
 
-export enum ParagraphItemType {
-  Unknown = 'unknown',
-  Error = 'error',
-  Paragraph = 'paragraph',
-  Span = 'span',
-  Bold = 'bold',
-  Italic = 'italic',
-  Text = 'text',
-  Variable = 'variable',
-  MatrixVariable = 'matrix-variable',
-  Linefeed = 'linefeed',
-  Color = 'color',
-  Itemize = 'itemize',
-  Enumerate = 'enumerate',
-  Reference = 'reference',
-  InlineMath = 'inline-math',
-  IntegerInput = 'integer-input',
-  MatrixInput = 'matrix-input',
+export abstract class MBL_LevelItem {
+  abstract toJSON(): JSONValue;
 }
 
-export class ParagraphItem extends DocumentItem {
-  type: ParagraphItemType;
-  subItems: ParagraphItem[] = [];
-  value = '';
-  constructor(type: ParagraphItemType) {
+// -------- SECTION --------
+
+export enum MBL_SectionType {
+  Section = 'section',
+  SubSection = 'subsection',
+  SubSubSection = 'subsubsection',
+}
+
+export class MBL_Section extends MBL_LevelItem {
+  type: MBL_SectionType;
+  text = '';
+  label = '';
+  constructor(type: MBL_SectionType) {
     super();
     this.type = type;
   }
   toJSON(): JSONValue {
-    const items: JSONValue = [];
-    for (const item of this.subItems) {
-      items.push(item.toJSON());
-    }
-    switch (this.type) {
-      case ParagraphItemType.Paragraph:
-      case ParagraphItemType.Span:
-      case ParagraphItemType.Bold:
-      case ParagraphItemType.Italic:
-      case ParagraphItemType.Itemize:
-      case ParagraphItemType.Enumerate:
-      case ParagraphItemType.InlineMath:
-        return {
-          type: this.type,
-          items: items,
-        };
-      case ParagraphItemType.Color:
-        return {
-          type: this.type,
-          value: this.value,
-          items: items,
-        };
-      case ParagraphItemType.Error:
-      case ParagraphItemType.Text:
-      case ParagraphItemType.Variable:
-      case ParagraphItemType.MatrixVariable:
-      case ParagraphItemType.Reference:
-        return {
-          type: this.type,
-          value: this.value,
-        };
-      case ParagraphItemType.Linefeed:
-      case ParagraphItemType.Unknown:
-        return {
-          type: this.type,
-        };
-      case ParagraphItemType.IntegerInput:
-      case ParagraphItemType.MatrixInput:
-        return {
-          type: this.type,
-          value: this.value,
-          width: 10, // TODO
-        };
-      default:
-        console.log('UNIMPLEMENTED toJSON(..) for "' + this.type + '"');
-        return {};
-    }
-  }
-  /**
-   * this methods concatenates text items
-   */
-  simplify(): void {
-    for (let i = 0; i < this.subItems.length; i++) {
-      if (
-        i > 0 &&
-        this.subItems[i - 1].type === ParagraphItemType.Text &&
-        this.subItems[i].type === ParagraphItemType.Text
-      ) {
-        let text = this.subItems[i].value;
-        if ('.,:!?'.includes(text) == false) text = ' ' + text;
-        this.subItems[i - 1].value += text;
-        // TODO: next line is an ugly hack for TeX..
-        this.subItems[i - 1].value = this.subItems[i - 1].value.replace(
-          /\\ /g,
-          '\\',
-        );
-        this.subItems.splice(i, 1);
-        i--;
-      } else {
-        this.subItems[i].simplify();
-      }
-    }
-  }
-}
-
-export class ExerciseInstance {
-  variables: SymTabEntry[] = [];
-  toJSON(): JSONValue {
-    const res: JSONValue = {};
-    for (const v of this.variables) {
-      switch (v.type.base) {
-        case BaseType.INT:
-          res[v.id] = (v.value as number).toString();
-          break;
-        case BaseType.MATRIX:
-          res[v.id] = (v.value as Matrix).toString();
-          break;
-        case BaseType.INT_SET:
-          res[v.id] = (v.value as Set_INT).toString();
-          break;
-        default:
-          console.log(
-            "WARNING: ExerciseInstance:toJSON(..): unimplemented type '" +
-              v.type.base +
-              "'",
-          );
-          break;
-      }
-    }
-    return res;
-  }
-}
-
-export enum TextLikeType {
-  Hidden = 'hidden',
-  Definition = 'definition',
-  Equation = 'equation',
-}
-
-export class TextLike extends DocumentItem {
-  type: TextLikeType;
-  value = ''; // text
-  error = '';
-  label = '';
-
-  toJSON(): JSONValue {
     return {
       type: this.type,
-      error: this.error,
-      value: this.value,
+      text: this.text,
       label: this.label,
     };
   }
 }
 
-/*export class Equation extends DocumentItem {
-  label = '';
-  text = '';
-  error = '';
+// -------- TEXT --------
 
+export abstract class MBL_Text extends MBL_LevelItem {}
+
+export class MBL_Text_Paragraph extends MBL_Text {
+  items: MBL_Text[] = [];
   toJSON(): JSONValue {
-    if (this.error.length > 0)
-      return {
-        type: 'equation',
-        error: this.error,
-      };
-    else
-      return {
-        type: 'equation',
-        label: this.label,
-        text: this.text,
-      };
-  }
-}*/
-
-export class Exercise extends DocumentItem {
-  title = '';
-  label = '';
-  code_raw = '';
-  text_raw = '';
-  instances: ExerciseInstance[] = [];
-  text: ParagraphItem = null;
-  error = '';
-
-  getVariable(id: string): SymTabEntry {
-    if (this.instances.length == 0) return null;
-    const inst = this.instances[0];
-    for (const v of inst.variables) {
-      if (v.id === id) return v;
-    }
-    return null;
-  }
-
-  toJSON(): JSONValue {
-    if (this.error.length > 0) {
-      return {
-        type: 'invalid-exercise',
-        title: this.title,
-        label: this.label,
-        error: this.error,
-      };
-    } else {
-      const vars: JSONValue = {};
-      if (this.instances.length > 0) {
-        const instance = this.instances[0];
-        for (const v of instance.variables) {
-          vars[v.id] = {
-            type: v.type.base,
-          };
-        }
-      }
-      const inst: JSONValue = [];
-      for (const i of this.instances) {
-        inst.push(i.toJSON());
-      }
-      if (this.text == null) {
-        const bp = 1337;
-      }
-      return {
-        type: 'exercise',
-        title: this.title,
-        label: this.label,
-        variables: vars,
-        instances: inst,
-        text: this.text.toJSON(),
-      };
-    }
+    return {
+      type: 'paragraph',
+      items: this.items.map((item) => item.toJSON()),
+    };
   }
 }
 
-/*export class Matrix {
-  rows = 1;
-  cols = 1;
-  values: number[] = [];
-
-  constructor(rows = 1, cols = 1) {
-    this.resize(rows, cols);
-  }
-
-  resize(rows: number, cols: number): void {
-    this.rows = rows;
-    this.cols = cols;
-    this.values = [];
-    const n = rows * cols;
-    for (let i = 0; i < n; i++) this.values.push(0);
-  }
-
-  setValue(row: number, col: number, value: number): void {
-    //TODO assert.ok(row >= 0 && row < this.rows);
-    //TODO assert.ok(col >= 0 && col < this.cols);
-    this.values[row * this.cols + col] = value;
-  }
-
-  getValue(row: number, col: number): number {
-    //TODO assert.ok(row >= 0 && row < this.rows);
-    //TODO assert.ok(col >= 0 && col < this.cols);
-    return this.values[row * this.cols + col];
-  }
-
+export class MBL_Text_InlineMath extends MBL_Text {
+  items: MBL_Text[] = [];
   toJSON(): JSONValue {
     return {
-      rows: this.rows,
-      cols: this.cols,
-      values: this.values,
+      type: 'inline_math',
+      items: this.items.map((item) => item.toJSON()),
     };
   }
-}*/
+}
 
-/*export class Variable {
-  name = '';
-  type: VariableType;
-
-  //TODO: better to NOT create arrays here???;
-
-  realValues: number[] = [];
-  boolValues: boolean[] = [];
-  matrixValues: Matrix[] = [];
-
+export class MBL_Text_Bold extends MBL_Text {
+  items: MBL_Text[] = [];
   toJSON(): JSONValue {
-    let values: JSONValue = [];
-    switch (this.type) {
-      case VariableType.Bool:
-        values = this.boolValues;
-        break;
-      case VariableType.Real:
-        values = this.realValues;
-        break;
-      case VariableType.Matrix:
-        for (const v of this.matrixValues) {
-          values.push(v.toJSON());
-        }
-        break;
-      default:
-        console.error('Variable.toJSON(..): unimplemented type ' + this.type);
-        process.exit(-1);
-    }
     return {
-      name: this.name,
+      type: 'bold',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_Italic extends MBL_Text {
+  items: MBL_Text[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'italic',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_Itemize extends MBL_Text {
+  items: MBL_Text[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'itemize',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_Enumerate extends MBL_Text {
+  items: MBL_Text[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'enumerate',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_EnumerateAlpha extends MBL_Text {
+  items: MBL_Text[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'enumerate_alpha',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_Span extends MBL_Text {
+  items: MBL_Text[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'span',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_AlignLeft extends MBL_Text {
+  items: MBL_Text[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'align_left',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_AlignCenter extends MBL_Text {
+  items: MBL_Text[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'align_center',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_AlignRight extends MBL_Text {
+  items: MBL_Text[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'align_right',
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export class MBL_Text_Text extends MBL_Text {
+  value = '';
+  toJSON(): JSONValue {
+    return {
+      type: 'text',
+      value: this.value,
+    };
+  }
+}
+
+export class MBL_Text_Linefeed extends MBL_Text {
+  toJSON(): JSONValue {
+    return {
+      type: 'linefeed',
+    };
+  }
+}
+
+export class MBL_Text_Color extends MBL_Text {
+  value = 0;
+  toJSON(): JSONValue {
+    return {
+      type: 'color',
+      value: this.value,
+    };
+  }
+}
+
+export class MBL_Text_Reference extends MBL_Text {
+  label = '';
+  toJSON(): JSONValue {
+    return {
+      type: 'reference',
+      label: this.label,
+    };
+  }
+}
+
+// -------- BLOCK ITEM --------
+
+export abstract class MBL_BlockItem extends MBL_LevelItem {
+  type: string;
+  title: string;
+  label: string;
+  error: string;
+}
+
+// -------- EQUATION --------
+
+export enum MBL_EquationOption {
+  AlignLeft = 'align_left',
+  AlignCenter = 'align_center',
+  AlignRight = 'align_right',
+  AlignEquals = 'align_equals',
+}
+
+export class MBL_Equation extends MBL_BlockItem {
+  value = '';
+  numbering = -1;
+  options: MBL_EquationOption[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'equation',
+      title: this.title,
+      label: this.label,
+      error: this.error,
+      value: this.value,
+      numbering: this.numbering,
+      options: this.options.map((option) => option.toString()),
+    };
+  }
+}
+
+// -------- DEFINITION --------
+
+export enum MBL_DefinitionType {
+  Definition = 'definition',
+  Theorem = 'theorem',
+  Lemma = 'lemma',
+  Corollary = 'corollary',
+  Proposition = 'proposition',
+  Conjecture = 'conjecture',
+  Axiom = 'axiom',
+  Claim = 'claim',
+  Identity = 'identity',
+  Paradox = 'paradox',
+}
+
+export class MBL_Definition extends MBL_BlockItem {
+  type: MBL_DefinitionType;
+  items: MBL_BlockItem[] = [];
+  constructor(type: MBL_DefinitionType) {
+    super();
+    this.type = type;
+  }
+  toJSON(): JSONValue {
+    return {
       type: this.type,
-      values: values,
+      title: this.title,
+      label: this.label,
+      error: this.error,
+      items: this.items.map((item) => item.toJSON()),
     };
   }
 }
 
-export enum VariableType {
+// -------- EXAMPLE --------
+
+export class MBL_Example extends MBL_BlockItem {
+  items: MBL_BlockItem[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'example',
+      title: this.title,
+      label: this.label,
+      error: this.error,
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+// -------- EXERCISE --------
+
+export class MBL_Exercise extends MBL_BlockItem {
+  variables: { [id: string]: MBL_Exercise_Variable } = {};
+  instances: MBL_Exercise_Instance[] = [];
+  text: MBL_Exercise_Text = new MBL_Text_Paragraph();
+  toJSON(): JSONValue {
+    const variablesJSON: { [id: string]: JSONValue } = {};
+    for (const v in this.variables) {
+      variablesJSON[v] = this.variables[v].toJSON();
+    }
+    return {
+      type: 'exercise',
+      title: this.title,
+      label: this.label,
+      error: this.error,
+      variables: variablesJSON,
+      instances: this.instances.map((instance) => instance.toJSON()),
+      text: this.text.toJSON(),
+    };
+  }
+}
+
+export enum MBL_Exercise_VariableType {
+  Int = 'int',
+  IntSet = 'int_set',
   Real = 'real',
-  Bool = 'bool',
+  RealSet = 'real_set',
+  Complex = 'complex',
+  ComplexSet = 'complex_set',
+  Vector = 'vector',
   Matrix = 'matrix',
 }
-*/
+
+export class MBL_Exercise_Variable {
+  type: MBL_Exercise_VariableType;
+  toJSON(): JSONValue {
+    return {
+      type: this.type.toString(),
+    };
+  }
+}
+
+export class MBL_Exercise_Instance {
+  value: { [id: string]: string } = {};
+  toJSON(): JSONValue {
+    return this.value;
+  }
+}
+
+export abstract class MBL_Exercise_Text extends MBL_Text {}
+
+export class MBL_Exercise_Text_Variable extends MBL_Exercise_Text {
+  variableId = '';
+  toJSON(): JSONValue {
+    return {
+      variable: this.variableId,
+    };
+  }
+}
+
+export class MBL_Exercise_Text_Input extends MBL_Exercise_Text {
+  type: MBL_Exercise_Text_Input_Type;
+  variable = '';
+  inputRequire: string[] = [];
+  inputForbid: string[] = [];
+  width = 0;
+  toJSON(): JSONValue {
+    return {
+      type: 'text_input',
+      input_type: this.type.toString(),
+      input_require: this.inputRequire.map((i) => i.toString()),
+      input_forbid: this.inputForbid.map((i) => i.toString()),
+      variable: this.variable,
+      width: this.width,
+    };
+  }
+}
+
+// TODO: export class MBL_Exercise_Text_Choices_Input
+
+export class MBL_Exercise_Text_Multiple_Choice extends MBL_Exercise_Text {
+  items: MBL_Exercise_Text_Single_or_Multi_Choice_Option[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'multiple_choice',
+      items: this.items.map((i) => i.toJSON()),
+    };
+  }
+}
+
+export class MBL_Exercise_Text_Single_or_Multi_Choice_Option {
+  variable = '';
+  text = '';
+  toJSON(): JSONValue {
+    return {
+      variable: this.variable,
+      text: this.text,
+    };
+  }
+}
+
+export enum MBL_Exercise_Text_Input_Type {
+  Int = 'int',
+  Real = 'real',
+  ComplexNormal = 'complex_normal',
+  ComplexPolar = 'complex_polar',
+  IntSet = 'int_set',
+  IntSetNArts = 'int_set_n_args',
+  Vector = 'vector',
+  VectorFlex = 'vector_flex',
+  Matrix = 'matrix',
+  MatrixFlexRows = 'matrix_flex_rows',
+  MatrixFlexCols = 'matrix_flex_cols',
+  MatrixFlex = 'matrix_flex',
+  Term = 'term',
+}
+
+// -------- FIGURE --------
+
+export class MBL_Figure extends MBL_BlockItem {
+  path = '';
+  caption = '';
+  options: MBL_Figure_Option[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'figure',
+      title: this.title,
+      label: this.label,
+      error: this.error,
+    };
+  }
+}
+
+export enum MBL_Figure_Option {
+  Width25 = 'width-25',
+  Width33 = 'width-33',
+  Width50 = 'width-50',
+  Width66 = 'width-66',
+  Width100 = 'width-100',
+}
+
+// -------- TABLE --------
+
+export class MBL_Table extends MBL_BlockItem {
+  head: string[] = [];
+  rows: MBL_Table_Row[] = [];
+  options: MBL_Table_Option[] = [];
+  toJSON(): JSONValue {
+    return {
+      type: 'table',
+      title: this.title,
+      label: this.label,
+      error: this.error,
+      head: this.head,
+      rows: this.rows.map((row) => row.toJSON()),
+      options: this.options.map((option) => option.toString()),
+    };
+  }
+}
+
+export class MBL_Table_Row {
+  text: string[] = [];
+  toJSON(): JSONValue {
+    return {
+      row: this.text,
+    };
+  }
+}
+
+export enum MBL_Table_Option {
+  AlignLeft = 'align_left',
+  AlignCenter = 'align_center',
+  AlignRight = 'align_right',
+}
+
+// -------- NEWPAGE --------
+
+export class MBL_NewPage extends MBL_LevelItem {
+  toJSON(): JSONValue {
+    return {
+      type: 'new_page',
+    };
+  }
+}
