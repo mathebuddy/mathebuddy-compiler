3fca96b349c95830c3964d9b09cd4e24a5c04958
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-08 12:43:17 +0100

    Update block.ts

--- src/block.ts
+++ src/block.ts
@@ -9,16 +9,19 @@
 // TODO: use npm-package in future!
 import * as SMPL from '@mathebuddy/mathebuddy-smpl/src';
 
+import { Compiler } from './compiler';
 import {
-  DocumentItem,
-  Exercise,
-  ExerciseInstance,
-  ParagraphItem,
-  ParagraphItemType,
-  TextLike,
-  TextLikeType,
-} from './data___old';
-import { Compiler } from './compiler___old';
+  MBL_Definition,
+  MBL_DefinitionType,
+  MBL_Equation,
+  MBL_Error,
+  MBL_LevelItem,
+  MBL_Text,
+  MBL_Text_AlignCenter,
+  MBL_Text_AlignLeft,
+  MBL_Text_AlignRight,
+  MBL_Text_Paragraph,
+} from './data';
 
 export class BlockPart {
   name = '';
@@ -29,7 +32,7 @@ export class Block {
   type = '';
   title = '';
   label = '';
-  parts: BlockPart[] = [];
+  parts: (MBL_LevelItem | BlockPart)[] = [];
   srcLine = 0;
 
   private parser: Compiler = null;
@@ -38,43 +41,123 @@ export class Block {
     this.parser = parser;
   }
 
-  process(): DocumentItem {
+  process(): MBL_LevelItem {
     switch (this.type) {
-      case 'HIDDEN':
-        return this.processTextLike(TextLikeType.Hidden);
       case 'DEFINITION':
-        return this.processTextLike(TextLikeType.Definition);
+        return this.processDefinition(MBL_DefinitionType.Definition);
+      case 'THEOREM':
+        return this.processDefinition(MBL_DefinitionType.Theorem);
+      case 'LEMMA':
+        return this.processDefinition(MBL_DefinitionType.Lemma);
+      case 'COROLLARY':
+        return this.processDefinition(MBL_DefinitionType.Corollary);
+      case 'PROPOSITION':
+        return this.processDefinition(MBL_DefinitionType.Proposition);
+      case 'CONJECTURE':
+        return this.processDefinition(MBL_DefinitionType.Conjecture);
+      case 'AXIOM':
+        return this.processDefinition(MBL_DefinitionType.Axiom);
+      case 'CLAIM':
+        return this.processDefinition(MBL_DefinitionType.Claim);
+      case 'IDENTITY':
+        return this.processDefinition(MBL_DefinitionType.Identity);
+      case 'PARADOX':
+        return this.processDefinition(MBL_DefinitionType.Paradox);
+
+      case 'LEFT':
+      case 'CENTER':
+      case 'RIGHT':
+        return this.processTextAlign(this.type);
+
       case 'EQUATION':
-        return this.processTextLike(TextLikeType.Equation);
-      case 'EXERCISE':
-        return this.processExercise();
+        return this.processEquation();
+
+      /*case 'EXERCISE':
+        return this.processExercise();*/
+
       default: {
-        const err = new ParagraphItem(ParagraphItemType.Error);
-        err.value = 'ERROR: unknown block type "' + this.type + '"';
-        const p = new ParagraphItem(ParagraphItemType.Paragraph);
-        p.subItems.push(err);
-        return p;
+        const err = new MBL_Error();
+        err.message = 'unknown block type "' + this.type + '"';
+        return err;
       }
     }
   }
 
-  private processTextLike(type: TextLikeType): TextLike {
-    const tl = new TextLike();
-    tl.type = type;
-    for (const part of this.parts) {
-      switch (part.name) {
-        case 'global':
-          tl.value = part.lines.join('\n');
-          break;
-        default:
-          tl.error = 'unknown part "' + part.name + '"';
-          break;
+  private processEquation(): MBL_Equation {
+    const equation = new MBL_Equation();
+    equation.title = this.title;
+    equation.label = this.label;
+    for (const p of this.parts) {
+      if (p instanceof BlockPart) {
+        const part = <BlockPart>p;
+        switch (part.name) {
+          case 'global':
+            equation.value = part.lines.join('\\\\');
+            break;
+          default:
+            equation.error += 'unexpected part "' + part.name + '"';
+            break;
+        }
+      } else {
+        equation.error += 'unexpected sub-block';
       }
     }
-    return tl;
+    return equation;
   }
 
-  private processExercise(): Exercise {
+  private processTextAlign(type: string): MBL_Text {
+    let align: MBL_Text_AlignLeft | MBL_Text_AlignCenter | MBL_Text_AlignRight;
+    switch (type) {
+      case 'LEFT':
+        align = new MBL_Text_AlignLeft();
+        break;
+      case 'CENTER':
+        align = new MBL_Text_AlignCenter();
+        break;
+      case 'RIGHT':
+        align = new MBL_Text_AlignRight();
+        break;
+    }
+    for (const p of this.parts) {
+      if (p instanceof BlockPart) {
+        const part = <BlockPart>p;
+        switch (part.name) {
+          case 'global':
+            align.items.push(this.parser.parseParagraph(part.lines.join('\n')));
+            break;
+        }
+      } else {
+        // TODO: check if allowed here!!
+        align.items.push(p);
+      }
+    }
+    return align;
+  }
+
+  private processDefinition(type: MBL_DefinitionType): MBL_Definition {
+    const def = new MBL_Definition(type);
+    def.title = this.title;
+    def.label = this.label;
+    for (const p of this.parts) {
+      if (p instanceof BlockPart) {
+        const part = <BlockPart>p;
+        switch (part.name) {
+          case 'global':
+            def.items.push(this.parser.parseParagraph(part.lines.join('\n')));
+            break;
+          default:
+            def.error += 'unexpected part "' + part.name + '"';
+            break;
+        }
+      } else {
+        // TODO: check if allowed here!!
+        def.items.push(p);
+      }
+    }
+    return def;
+  }
+
+  /*private processExercise(): Exercise {
     const exercise = new Exercise();
     exercise.title = this.title;
     for (const part of this.parts) {
@@ -108,9 +191,9 @@ export class Block {
     }
     exercise.text = this.parser.parseParagraph(exercise.text_raw, exercise);
     return exercise;
-  }
+  }*/
 
   private error(message: string): void {
-    console.error('ERROR:' + (this.srcLine + 1) + ': ' + message);
+    console.error('' + (this.srcLine + 1) + ': ' + message);
   }
 }
