5cfddd44947ff05682b3a8bbe872ad602eb161c5
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-12 13:10:00 +0100

    Create dataLevel.ts

---
+++ src/dataLevel.ts
@@ -0,0 +1,40 @@
+/**
+ * mathe:buddy - eine gamifizierte Lern-App für die Hoehere Mathematik
+ * (c) 2022 by TH Koeln
+ * Author: Andreas Schwenk contact@compiler-construction.com
+ * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+ * License: GPL-3.0-or-later
+ */
+
+import { JSONValue } from './dataJSON';
+
+// refer to the specification at https://app.f07-its.fh-koeln.de/docs-mbcl.html
+
+// -------- LEVEL --------
+
+export class MBL_Level {
+  title = '';
+  label = '';
+  pos_x = -1;
+  pos_y = -1;
+  requires: MBL_Level[] = [];
+  items: MBL_LevelItem[] = [];
+  postProcess(): void {
+    for (const i of this.items) i.postProcess();
+  }
+  toJSON(): JSONValue {
+    return {
+      title: this.title,
+      label: this.label,
+      pos_x: this.pos_x,
+      pos_y: this.pos_y,
+      requires: this.requires.map((req) => req.title),
+      items: this.items.map((item) => item.toJSON()),
+    };
+  }
+}
+
+export abstract class MBL_LevelItem {
+  abstract postProcess(): void;
+  abstract toJSON(): JSONValue;
+}
