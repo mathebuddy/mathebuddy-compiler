734d07c8b395c73eda1e174a650bf7bdeb77a81e
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-12 13:09:57 +0100

    Create dataJSON.ts

---
+++ src/dataJSON.ts
@@ -0,0 +1,16 @@
+/**
+ * mathe:buddy - eine gamifizierte Lern-App für die Hoehere Mathematik
+ * (c) 2022 by TH Koeln
+ * Author: Andreas Schwenk contact@compiler-construction.com
+ * Funded by: FREIRAUM 2022, Stiftung Innovation in der Hochschullehre
+ * License: GPL-3.0-or-later
+ */
+
+// refer to the specification at https://app.f07-its.fh-koeln.de/docs-mbcl.html
+
+export type JSONValue =
+  | string
+  | number
+  | boolean
+  | { [x: string]: JSONValue }
+  | Array<JSONValue>;
