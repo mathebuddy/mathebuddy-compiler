6fff1712aa5cf9e7e4b1766854e07d61a1c3827f
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-12-13 12:56:07 +0100

    Update dataLevel.ts

--- src/dataLevel.ts
+++ src/dataLevel.ts
@@ -13,22 +13,27 @@ import { JSONValue } from './dataJSON';
 // -------- LEVEL --------
 
 export class MBL_Level {
+  file_id = ''; // all references go here; label is only used for searching
   title = '';
   label = '';
   pos_x = -1;
   pos_y = -1;
   requires: MBL_Level[] = [];
+  requires_tmp: string[] = []; // only used while compiling
   items: MBL_LevelItem[] = [];
+
   postProcess(): void {
     for (const i of this.items) i.postProcess();
   }
+
   toJSON(): JSONValue {
     return {
+      file_id: this.file_id,
       title: this.title,
       label: this.label,
       pos_x: this.pos_x,
       pos_y: this.pos_y,
-      requires: this.requires.map((req) => req.label),
+      requires: this.requires.map((req) => req.file_id),
       items: this.items.map((item) => item.toJSON()),
     };
   }
